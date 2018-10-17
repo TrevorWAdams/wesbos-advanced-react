f41ceec16ea611c0040d07b91a737c90506694a4
Trevor W Adams <trevorwadams@Trevors-MacBook-Pro.local>
2018-10-17 07:37:40 -0600

    Lesson 19

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import CreateItem from '../components/CreateItem';
 
 const sell = () => {
   return (
     <div>
-      <p>Sell</p>
+      <CreateItem />
     </div>
   );
 };
