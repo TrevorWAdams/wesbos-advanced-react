8796e63bdaa9579701d555806d5060448b735f98
Trevor W Adams <trevorwadams@Trevors-MacBook-Pro.local>
2018-10-18 16:52:39 -0600

    Lesson 24

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -7,8 +7,8 @@ import Pagination from './Pagination';
 import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(skip: $skip, first: $first, orderBy: createdAt_DESC) {
       id
       title
       description
@@ -36,7 +36,13 @@ class Items extends Component {
     return (
       <Center>
         <Pagination page={this.props.page}/>
-        <Query query={ALL_ITEMS_QUERY}>
+        <Query
+          query={ALL_ITEMS_QUERY}
+          // fetchPolicy="network-only"
+          variables={{
+            skip: this.props.page * perPage - perPage
+          }}
+        >
           {({ data, error, loading }) => {
             if (loading) return <p>loading ..</p>;
             if (error) return <p>Error: {error.message}</p>;
