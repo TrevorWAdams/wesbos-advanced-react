40c29289c544e0e68334d65606a31a4f35fef06b
Trevor W Adams <trevorwadams@Trevors-MacBook-Pro.local>
2018-10-19 13:15:00 -0600

    lesson 25

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,12 +1,12 @@
+const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middleware to handle cookies (JWT)
+server.express.use(cookieParser());
 // TODO Use express middleware to populate current user
-
 
 server.start(
   {
@@ -17,5 +17,5 @@ server.start(
   },
   deets => {
     console.log(`Server is now running on port http://localhost:${deets.port}`);
-  }
+  },
 );
--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
@@ -34,7 +37,33 @@ const Mutations = {
     // Check if they own the item or has permissions to delete
     //TODO: Implement
     // Delete it
-    return ctx.db.mutation.deleteItem({ where}, info);
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    );
+    // create the JWT token for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // We set the jwt as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // Finally we return the user to the browser
+    return user;
   },
 };
 
